9436ebf37596729ff15c014cf64f5026ea1ede5b
Ewa Ram <ekaramus@github.com>
2020-09-11 12:47:56 +0200

    add blackage in the edges

--- js/tetris-js/board.js
+++ js/tetris-js/board.js
@@ -1,5 +1,5 @@
 class Board {
-    
+
     constructor(ctx) {
       this.ctx = ctx;
       this.grid = this.getEmptyBoard();
@@ -11,4 +11,29 @@ class Board {
         {length: ROWS}, () => Array(COLS).fill(0)
       );
     }
+
+    valid(p) {
+        return p.shape.every((row, dy) => {
+          return row.every((value, dx) => {
+            let x = p.x + dx;
+            let y = p.y + dy;
+            return (
+              value === 0 ||
+              (this.insideWalls(x) && this.aboveFloor(y) && this.notOccupied(x, y))
+            );
+          });
+        });
+      }
+
+    insideWalls(x) {
+        return x >= 0 && x < COLS;
+    }
+    
+    aboveFloor(y) {
+        return y <= ROWS;
+    }
+    notOccupied(x, y) {
+        return this.grid[y] && this.grid[y][x] === 0;
+      }
+
   }
