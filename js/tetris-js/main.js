f6e31a3da6a9a0aa950a6be3b264161925cf6d5d
Ewa Ram <ekaramus@github.com>
2020-09-11 10:16:31 +0200

    add initialize board function

--- js/tetris-js/main.js
+++ js/tetris-js/main.js
@@ -1,10 +1,20 @@
 const gameWindow = document.getElementById("game-board");
 const context = gameWindow.getContext("2d");
 
+let board;
+
+
 //Calculating the size of board with the params given in constants.js file
 context.gameWindow.width = COLS *BLOCK_SIZE;
 context.gameWindow.height = ROWS *BLOCK_SIZE;
 
 //Setting the scale for the board
 context.scale(BLOCK_SIZE);
+
+//Initializing the board
+function play() {
+    board = new Board(context);
+    //checking if the table is initialized correctly
+    console.table(board.grid);
+}
 
