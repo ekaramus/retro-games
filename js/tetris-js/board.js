c3ce2406712c5d062af9ac0c8d6e097fd3c8ccc7
Ewa Ram <ekaramus@github.com>
2020-09-11 13:18:55 +0200

    add rotation of the piece

--- js/tetris-js/board.js
+++ js/tetris-js/board.js
@@ -34,6 +34,27 @@ class Board {
     }
     notOccupied(x, y) {
         return this.grid[y] && this.grid[y][x] === 0;
-      }
+    }
+
+    rotate(p) {
+        //create a copy to transform and than switch return in place of the original
+        let copy = JSON.parse(JSON.stringify(p));
+
+        //rotate the matrix
+        // Transpose matrix, p is the Piece.
+        for (let y = 0; y < p.shape.length; ++y) {
+            for (let x = 0; x < y; ++x) {
+              [p.shape[x][y], p.shape[y][x]] = 
+              [p.shape[y][x], p.shape[x][y]];
+            }
+        }
+            
+        // Reverse the order of the columns.
+        p.shape.forEach(row => row.reverse());
+
+        console.log(p);
+
+        return copy;
+    }
 
   }
