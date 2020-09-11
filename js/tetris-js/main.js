a68e9c64c5e57afe9a80f05fe350049ee19f5a3a
Ewa Ram <ekaramus@github.com>
2020-09-11 10:00:57 +0200

    rename the board to gameWindow to prevent mismatch with the Board class

--- js/tetris-js/main.js
+++ js/tetris-js/main.js
@@ -1,9 +1,10 @@
-const board = document.getElementById("game-board");
-const context = board.getContext("2d");
+const gameWindow = document.getElementById("game-board");
+const context = gameWindow.getContext("2d");
 
 //Calculating the size of board with the params given in constants.js file
-context.board.width = COLS *BLOCK_SIZE;
-context.board.height = ROWS *BLOCK_SIZE;
+context.gameWindow.width = COLS *BLOCK_SIZE;
+context.gameWindow.height = ROWS *BLOCK_SIZE;
 
 //Setting the scale for the board
 context.scale(BLOCK_SIZE);
+
