924874f54dea52d52b44d393de01e52b0e81c67d
Ewa Ram <ekaramus@github.com>
2020-09-11 11:42:49 +0200

    change name from context to ctx to make the code work

--- js/tetris-js/main.js
+++ js/tetris-js/main.js
@@ -1,20 +1,22 @@
 const canvas = document.getElementById("gameBoard");
-const context = canvas.getContext("2d");
+const ctx = canvas.getContext("2d");
 
 let board;
 
 
 //Calculating the size of board with the params given in constants.js file
-context.canvas.width = COLS * BLOCK_SIZE;
-context.canvas.height = ROWS *BLOCK_SIZE;
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS *BLOCK_SIZE;
 
 //Setting the scale for the board
-context.scale(BLOCK_SIZE, BLOCK_SIZE);
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
 //Initializing the board
 function play() {
-    board = new Board(context);
-    //checking if the table is initialized correctly
-    console.table(board.grid);
+  board = new Board(ctx);
+  let piece = new Piece(ctx);
+  piece.draw();
+
+  board.piece = piece;
 }
 
